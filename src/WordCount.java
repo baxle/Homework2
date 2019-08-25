b788bc1e5ad7c780fd13e721292727f918e05638
baxle <50596760+baxle@users.noreply.github.com>
2019-08-25 16:57:15 +0300

    Add files via upload

---
+++ src/WordCount.java
@@ -0,0 +1,92 @@
+
+import java.io.*;
+import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.StringTokenizer;
+
+public class WordCount {
+
+    public static void main(String[] args) throws IOException {
+
+        //Creating BufferedReader to accept the file name from the user
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = null;
+        int maxValue=0;
+
+        System.out.println("Нужно ввести путь к файлу.");
+        System.out.println("Можно ввести абсолютный путь к файлу.");
+        System.out.println("Можно ввести название файла, находящегося в директории проекта. (Text.txt уже лежит в директории)");
+
+        fileName = (String) br.readLine();
+
+
+        if (fileName.isEmpty()) {
+            System.err.println("Вы ничего не ввели");
+            br.close();
+        }
+        else
+            try {
+                //Creating the BufferedReader to read the file
+                File textFile = new File(fileName);
+                BufferedReader input = new BufferedReader(new FileReader(textFile));
+
+                //Creating the Map to store the words and their occurrences
+                TreeMap<String, Integer> frequencyMap = new TreeMap<String, Integer>();
+                String currentLine = null;
+
+                //Reading line by line from the text file
+                while ((currentLine = input.readLine()) != null) {
+
+                    //Parsing the words from each line
+                    StringTokenizer parser = new StringTokenizer(currentLine, " \t\n\r\f.,;:!?'\"1234567890()");
+                    while (parser.hasMoreTokens()) {
+                        String currentWord = parser.nextToken().toLowerCase();
+                        Integer frequency = frequencyMap.get(currentWord);
+                        if (frequency == null) {
+                            frequency = 0;
+                        }
+                        //Putting each word and its occurrence into Map
+                        frequencyMap.put(currentWord, frequency + 1);
+                    }
+
+                }
+
+                for (String key : frequencyMap.keySet()) {
+                    System.out.print(key + " ");
+                }
+                System.out.println(" ");
+                System.out.println(frequencyMap);
+                maxValue = Collections.max(frequencyMap.values());
+
+                System.out.println("Максимально встречаются (частота " + maxValue + ") следующие слова: ");
+                for (Map.Entry<String, Integer> entry : frequencyMap.entrySet()) {
+                    if (entry.getValue().equals(maxValue)) {
+                        System.out.println(entry.getKey());
+                    }
+                }
+
+            }
+
+            catch (FileNotFoundException e){
+                System.err.println("Файл не найден!");
+            }
+
+
+            catch (IOException ie) {
+
+            }
+            finally {
+                try {
+                    if (br!=null){
+                        br.close();
+                    }
+                }
+                catch (IOException e){
+
+                }
+            }
+    }
+}
+
+
